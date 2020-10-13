52d5aacac166be7b3c635e52601cfa50469e8f6b
moaaz-bhnas <moaaz_bs@yahoo.com>
2020-10-13 23:01:36 +0200

    before trying to separate auth logic from pages

--- src/pages/[lang]/add-product.js
+++ src/pages/[lang]/add-product.js
@@ -1,6 +1,11 @@
 import AddProductForm from "../../components/addProductForm";
 import Layout from "../../components/layout";
 
+export async function getStaticProps(context) {
+  const locale = getLocaleInServer(context);
+  await checkAuthInServer(locale, context);
+}
+
 const AddProduct = () => {
   return (
     <Layout>
